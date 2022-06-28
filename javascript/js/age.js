fdd9f6ecfc297c3488e68b0e813ffd767d0e3925
KSY0606 <tkddbs9506@naver.com>
2022-06-28 19:35:47 +0900

    자바스크립트

---
+++ javascript/js/age.js
@@ -0,0 +1,14 @@
+function calc() {
+    var currnetYear = 2022;
+    var birthYear = prompt("태어난 연도를 입력하세요", "yyyy");
+    var age;
+    var result;
+    
+    if(isNaN(birthYear)) {
+        result="입력값이 없어 나이 알 수 없음"
+    }else {
+        age = currnetYear-birthYear+1;
+        result = "당신의 나이는" + age + "세 입니다.";
+    }
+    document.querySelector("#result").innerHTML = result;
+}
