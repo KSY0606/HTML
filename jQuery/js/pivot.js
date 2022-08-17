d1152f60880bb6db6f1501be2c135d75f83bb7d8
KSY0606 <tkddbs9506@naver.com>
2022-08-17 22:37:48 +0900

    플러그인 테스트

---
+++ jQuery/js/pivot.js
@@ -0,0 +1,74 @@
+$.fn.pivot = function(options) {
+    // 변수를 선언
+    let $target = $(this);
+    let $items = $target.children();
+    let $container = $target.wrap("<div></div>").parent();
+    let option = {
+        width : 500,
+        height : 450
+    };
+    // 옵션 처리
+    $.extend(option, options);
+    // 스타일 지정
+    $target.css({
+        width : $items.length*option.width,
+        height : option.height,
+        position : "absolute",
+    });
+    $items.css({
+        width : option.width,
+        height : option.height,
+        float : "left"
+    });
+    $container.css({
+        width : option.width,
+        height : option.height,
+        position : "relative",
+        overflow : "hidden"
+    });
+    // 이벤트 연결(플러그인 동작)
+    let originLeft = 0;
+    let oldLeft = 0;
+    let nowPosition = 0;
+    let isDown = false;
+    $target.on({
+        mousedown : function(event) {
+            isDown = true;
+            oldLeft = originLeft = event.clientX;
+            event.preventDefault();
+        },
+        mousemove : function(event) {
+            if(isDown) {
+                let distance = oldLeft - event.clientX;
+                oldLeft = event.clientX;
+                // 움직임을 구현
+                $target.animate({
+                    left : "-=" + distance
+                },0);
+                $target.stop(true);
+            }
+            event.preventDefault();
+        },
+        mouseup : function(event) {
+            // 내부 함수
+            function movePosition(direction) {
+                // 위치를 설정
+                let changePosition = nowPosition + direction;
+                if(changePosition >= 0 && changePosition < $items.length) {
+                    nowPosition = changePosition;
+                }
+            }
+            // 요소의 1/4 이상 드래그 했을 경우 피벗
+            if(originLeft - event.clientX > option.width/4) {
+                movePosition(+1);
+            }else if(originLeft - event.clientX < -option.width/4) {
+                movePosition(-1);
+            }
+            $target.animate({
+                left : -nowPosition*option.width
+            },500);
+            isDown = false;
+            event.preventDefault();
+        }
+    })
+}
---
+++ jQuery/js/reverse.js
@@ -0,0 +1,4 @@
+$.fn.sangyun=function(options) {
+    $(this).css("color", "#fff");
+    $(this).css("background", "black");
+}
